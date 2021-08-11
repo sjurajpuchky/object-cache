75e9b7f2b8ba6b24ccaa2ec795eccc05d037f458
Juraj Puchký <jpuchky@paralix.baba.bj>
2021-08-11 19:51:16 +0200

    default ttl sample disk

--- samples/disk.php
+++ samples/disk.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $object = [1,2,3,4];
 
-$cache = new \BABA\Cache\Cache(new \BABA\Cache\Drivers\Disk());
+$cache = new \BABA\Cache\Cache(new \BABA\Cache\Drivers\Disk(), 3600 * 24);
 
 $cache->store('test', $object);
 var_dump($cache->load('test'));
